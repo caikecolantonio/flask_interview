a1e5f5b35b50bb9f8c80c7c3af18d87dc115b56e
Caike <caikecolantonio@hotmail.com>
2020-09-23 16:56:41 -0300

    deploy

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
 
 
-def test_requests_returns_404(client):
+def test_requests_retorna_404(client):
     assert client.get("/url_nao_existe").status_code == 404
+
+def test_config_foi_carregada(config):
+    assert config["DEBUG"] is False
+

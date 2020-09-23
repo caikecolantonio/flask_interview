b07d75eb9161f75cbb3305fff14603b2a8e74145
Caike <caikecolantonio@hotmail.com>
2020-09-23 00:38:17 -0300

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 from .model import configure as config_db
 from .serializer import configure as config_ma
 
-def create_app():
-    app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    config_db(app)
-    config_ma(app)
+app = Flask(__name__)
 
-    Migrate(app, app.db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    from .usuarios import bp_usuarios
-    app.register_blueprint(bp_usuarios)
-    return app
+config_db(app)
+config_ma(app)
+
+Migrate(app, app.db)
+
+from .usuarios import bp_usuarios
+app.register_blueprint(bp_usuarios)
+
+
+@app.route('/get', methods = ['GET'])
+def get_teste():
+    return jsonify({'Hello':'World'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/usuarios.py
+++ app/usuarios.py
@@ -6,9 +6,8 @@ bp_usuarios = Blueprint('usuarios', __name__)
 
 @bp_usuarios.route('/mostrar', methods=['GET'])
 def mostrar():
-    bs = UsuarioSchema(many=True)
     result = Usuario.query.all()
-    return bs.jsonify(result), 200
+    return UsuarioSchema(many=True).jsonify(result), 200
 
 @bp_usuarios.route('/deletar/<identificador>', methods=['GET'])
 def deletar(identificador):
@@ -27,7 +26,13 @@ def modificar(identificador):
 @bp_usuarios.route('/cadastrar', methods=['POST'])
 def cadastrar():
     bs = UsuarioSchema()
-    usuario, error = bs.load(request.json)
+    usuario = bs.load(request.json)
+    nome_completo = request.json.get('nome_completo', '')
+    cpf = request.json.get('cpf', '')
+    email = request.json.get('email', '')
+
+    usuario = Usuario(nome_completo=nome_completo, cpf=cpf, email=email)
+
     current_app.db.session.add(usuario)
     current_app.db.session.commit()
 
